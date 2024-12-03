aa4cba80200ed245f36cb67b9bdca205bdce486c
yugo0106 <y.f.13@outlook.com>
2024-12-03 12:24:58 +0900

    ecsite

---
+++ src/main/java/jp/co/internous/ecsite/model/domain/MstGoods.java
@@ -0,0 +1,43 @@
+package jp.co.internous.ecsite.model.domain;
+import java.sql.Timestamp;
+
+public class MstGoods {
+
+	private int id;
+	private String goodsName;
+	private int price;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id= id;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Timestamp getUpdateAt() {
+		return updatedAt;
+	}
+	public void setUpdateAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
+ 
---
+++ src/main/java/jp/co/internous/ecsite/model/form/HistoryForm.java
@@ -0,0 +1,13 @@
+package jp.co.internous.ecsite.model.form;
+
+public class HistoryForm {
+
+	private int userId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+}
